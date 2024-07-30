881d427005a2d9e73c8d3db416b314c7a5c8beea
Krishna1081 <90314846+Krishna1081@users.noreply.github.com>
2024-07-31 00:03:23 +0530

    update 3 for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+echo "build start"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "build end"
--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t7!-5wv%u49%insple4c(i)190=u6qhe-o=qb_*w=qwi##g5ey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
